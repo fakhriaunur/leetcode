3e03344945db77256053dabeda81f857c9e66d64
Fakhri A <fakhri.aunur@gmail.com>
2023-12-23 00:25:44 +0700

    solve 704.binary search

---
+++ 704.binary-search.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if target < nums[mid]:
+                right = mid - 1
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                return mid
+        return -1
+        
+# @lc code=end
+
