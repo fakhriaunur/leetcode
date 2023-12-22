41d34feaa3c2298b760f60d617f3cfa527c9e3b6
Fakhri A <fakhri.aunur@gmail.com>
2023-12-22 21:27:36 +0700

    solve 226 invert binary tree

---
+++ 226.invert-binary-tree.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=226 lang=python3
+#
+# [226] Invert Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import Optional
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def invert(node: Optional[TreeNode]):
+            if not node:
+                return
+                
+            node.left, node.right = node.right, node.left
+                
+            invert(node.left)
+            invert(node.right)
+            
+        invert(root)
+        
+        return root
+        
+# @lc code=end
+
