9dbbcbb79af10275eba6af2fa9a22ddd4352735e
Fakhri A <fakhri.aunur@gmail.com>
2023-12-24 00:19:20 +0700

    solve 141. linked list cycle

---
+++ 141.linked-list-cycle.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=141 lang=python3
+#
+# [141] Linked List Cycle
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+from typing import Optional
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+            if slow == fast:
+                return True
+        
+        return False
+        
+# @lc code=end
+
