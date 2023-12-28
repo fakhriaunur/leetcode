2664ab2e5ef6a92b9881e568e330d4596eaef1c0
Fakhri A <fakhri.aunur@gmail.com>
2023-12-28 16:30:25 +0700

    solve 15. 3 sum

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -12,10 +12,10 @@ class Solution:
         n = len(nums)
         nums.sort()
         
-        for i in range(n - 2):
+        for i in range(n-2):
             if nums[i] > 0:
                 break
-                
+            
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
@@ -35,10 +35,10 @@ class Solution:
                     right -= 1
                     
                     while left < right and nums[left] == nums[left-1]:
-                            left += 1
-                        
+                        left += 1
+                    
                     while left < right and nums[right] == nums[right+1]:
-                            right -= 1
+                        right -= 1
         
         return triplet
         
