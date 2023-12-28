b0f24b139816e99b8986eab81eec5f89ef24f592
Fakhri A <fakhri.aunur@gmail.com>
2023-12-28 16:21:26 +0700

    solve 15. 3 sum

---
+++ 15.3-sum.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        triplet = []
+        n = len(nums)
+        nums.sort()
+        
+        for i in range(n - 2):
+            if nums[i] > 0:
+                break
+                
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            left = i + 1
+            right = n - 1
+            
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+                
+                if curr_sum < 0:
+                    left += 1
+                elif curr_sum > 0:
+                    right -= 1
+                else:
+                    triplet.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    
+                    while left < right and nums[left] == nums[left-1]:
+                            left += 1
+                        
+                    while left < right and nums[right] == nums[right+1]:
+                            right -= 1
+        
+        return triplet
+        
+# @lc code=end
+
