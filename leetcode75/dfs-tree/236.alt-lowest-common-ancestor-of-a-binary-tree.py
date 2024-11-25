1d4d531f165b65a1c7a5565085f02ee6509d912d
Fakhri <fakhri.aunur@gmail.com>
2024-11-25 15:41:41 +0700

    add alt to 236. lca of a binary tree

---
+++ leetcode75/dfs-tree/236.alt-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=236 lang=python3
+#
+# [236] Lowest Common Ancestor of a Binary Tree
+#
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# @lc code=start
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+            
+        if root in [p, q]:
+            return root
+        
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        
+        return left if left else right
+        
+# @lc code=end
+
